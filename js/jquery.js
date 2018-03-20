342157e18a4427221348a33ee72c06ce8a9a7f3f
Unknown <niklaslykke96@gmail.com>
2018-03-21 00:38:48 +0100

    Prototype 1

---
+++ js/jquery.js
@@ -0,0 +1,264 @@
+$(document).ready(function(){
+    
+    $('#intro-touch').click(function(){
+        $('#screensaver').fadeOut(1000);
+    });
+
+    $('.ceres, .ceres-gylden, .toersleff').click(function(){
+        $(this).css({
+            'width' : '90%',
+            'position' : 'absolute',
+            'margin' : '5%',
+            'margin-top' : '7.5%',
+            'z-index' : '2'
+        });
+        
+        $('#bg-overlay').css({
+            'display' : 'inherit'
+        });
+    });
+    
+    // Ceres Øl Plakat
+    $('.ceres').click(function(){
+        $('#ceres-touch').css({
+            'display' : 'inherit',
+            'z-index' : '2',
+        });
+        
+        $('.a').css({
+            'display' : 'inherit',
+        });
+    });
+    
+    // Ceres Gylnde Drik PLakat
+    $('.ceres-gylden').click(function(){
+        $('#gylden-touch').css({
+            'display' : 'inherit',
+            'z-index' : '2',
+        });
+        
+        $('.b').css({
+            'display' : 'inherit',
+        });
+    });
+    
+        // Ceres Gyldne Drik Textbox
+        // Textbox D
+        $('.gylden-a').click(function(){
+            $('.ceres-gylden').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-d, .return-d, .txtbox-d').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+    
+        // Textbox E
+        $('.gylden-b').click(function(){
+            $('.ceres-gylden').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-e, .return-e, .txtbox-e').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+    
+        // Textbox F
+        $('.gylden-c').click(function(){
+            $('.ceres-gylden').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-f, .return-f, .txtbox-f').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+    
+    // Tørsleff Vanilie Plakat
+    $('.toersleff').click(function(){
+        $('#toersleff-touch').css({
+            'display' : 'inherit',
+            'z-index' : '2',
+        });
+        
+        $('.c').css({
+            'display' : 'inherit',
+        });
+    });
+    
+        // Tørsleff Vanilie Textbox
+        // Textbox G
+        $('.toersleff-a').click(function(){
+            $('.toersleff').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-g, .return-g, .txtbox-g').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+    
+        // Textbox H
+        $('.toersleff-b').click(function(){
+            $('.toersleff').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-h, .return-h, .txtbox-h').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+    
+        // Textbox I
+        $('.toersleff-c').click(function(){
+            $('.toersleff').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-i, .return-i, .txtbox-i').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+    
+        // Textbox J
+        $('.toersleff-d').click(function(){
+            $('.toersleff').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-j, .return-j, .txtbox-j').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+
+    // Close Button
+    $('.close').click(function(){
+        $('.ceres, .ceres-gylden, .toersleff, #ceres-touch, #gylden-touch, #toersleff-touch, #bg-overlay, .close').removeAttr('style')
+    });
+    
+    // Return Button
+    $('.return').click(function(){
+        $('.close, .rotation').css({
+            'display' : 'inherit',
+            'z-index' : '2'
+        });
+        
+        $('#bg-overlay').css({
+            'background-color' : 'rgba(0, 0, 0, 0.5)'
+        });
+        
+        $('.frame, .return, .txtbox').css({
+            'display' : 'none'
+        });
+    });
+    
+    $('.return-a, .return-b, .return-c').click(function(){
+        $('.ceres').css({
+            'display' : 'inherit',
+            'z-index' : '2'
+        });
+    });
+    
+    $('.return-d, .return-e, .return-f').click(function(){
+        $('.ceres-gylden').css({
+            'display' : 'inherit',
+            'z-index' : '2'
+        });
+    });
+    
+    $('.return-g, .return-h, .return-i, .return-j').click(function(){
+        $('.toersleff').css({
+            'display' : 'inherit',
+            'z-index' : '2'
+        });
+    });
+    
+    // Idle Timer
+    
+});
