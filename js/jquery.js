5a5c9f4615fd4faa7a2a8d6dea43bc821de90045
Unknown <niklaslykke96@gmail.com>
2018-03-21 16:44:42 +0100

    Semi-final
    Næsten done done

--- js/jquery.js
+++ js/jquery.js
@@ -30,6 +30,100 @@ $(document).ready(function(){
         });
     });
     
+        // Ceres Øl Textbox
+        // Textbox A
+        $('.ceres-a').click(function(){
+            $('.ceres').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-a, .return-a, .txtbox-a').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+    
+        // Textbox B
+        $('.ceres-b').click(function(){
+            $('.ceres').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-b, .return-b, .txtbox-b').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+    
+        // Textbox C
+        $('.ceres-c').click(function(){
+            $('.ceres').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-c, .return-c, .txtbox-c').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+    
+        // Textbox D
+        $('.ceres-d').click(function(){
+            $('.ceres').css({
+                'z-index' : '1'
+            });
+            
+            $('.frame-d, .return-d, .txtbox-d').css({
+                'display' : 'inherit'
+            });
+            
+            $('.close').css({
+                'display' : 'none'
+            });
+            
+            $('#bg-overlay').css({
+                'background-color' : 'rgba(0, 0, 0, 0.7)'
+            });
+            
+            $('.rotation').css({
+                'display' : 'none'
+            });
+        });
+        
+    
     // Ceres Gylnde Drik PLakat
     $('.ceres-gylden').click(function(){
         $('#gylden-touch').css({
@@ -49,7 +143,7 @@ $(document).ready(function(){
                 'z-index' : '1'
             });
             
-            $('.frame-d, .return-d, .txtbox-d').css({
+            $('.frame-e, .return-e, .txtbox-e').css({
                 'display' : 'inherit'
             });
             
@@ -72,7 +166,7 @@ $(document).ready(function(){
                 'z-index' : '1'
             });
             
-            $('.frame-e, .return-e, .txtbox-e').css({
+            $('.frame-f, .return-f, .txtbox-f').css({
                 'display' : 'inherit'
             });
             
@@ -95,7 +189,7 @@ $(document).ready(function(){
                 'z-index' : '1'
             });
             
-            $('.frame-f, .return-f, .txtbox-f').css({
+            $('.frame-g, .return-g, .txtbox-g').css({
                 'display' : 'inherit'
             });
             
@@ -131,7 +225,7 @@ $(document).ready(function(){
                 'z-index' : '1'
             });
             
-            $('.frame-g, .return-g, .txtbox-g').css({
+            $('.frame-h, .return-h, .txtbox-h').css({
                 'display' : 'inherit'
             });
             
@@ -154,7 +248,7 @@ $(document).ready(function(){
                 'z-index' : '1'
             });
             
-            $('.frame-h, .return-h, .txtbox-h').css({
+            $('.frame-i, .return-i, .txtbox-i').css({
                 'display' : 'inherit'
             });
             
@@ -177,7 +271,7 @@ $(document).ready(function(){
                 'z-index' : '1'
             });
             
-            $('.frame-i, .return-i, .txtbox-i').css({
+            $('.frame-j, .return-j, .txtbox-j').css({
                 'display' : 'inherit'
             });
             
@@ -200,7 +294,7 @@ $(document).ready(function(){
                 'z-index' : '1'
             });
             
-            $('.frame-j, .return-j, .txtbox-j').css({
+            $('.frame-k, .return-k, .txtbox-k').css({
                 'display' : 'inherit'
             });
             
@@ -238,21 +332,21 @@ $(document).ready(function(){
         });
     });
     
-    $('.return-a, .return-b, .return-c').click(function(){
+    $('.return-a, .return-b, .return-c, .return-d').click(function(){
         $('.ceres').css({
             'display' : 'inherit',
             'z-index' : '2'
         });
     });
     
-    $('.return-d, .return-e, .return-f').click(function(){
+    $('.return-e, .return-f, .return-g').click(function(){
         $('.ceres-gylden').css({
             'display' : 'inherit',
             'z-index' : '2'
         });
     });
     
-    $('.return-g, .return-h, .return-i, .return-j').click(function(){
+    $('.return-h, .return-i, .return-j, .return-k').click(function(){
         $('.toersleff').css({
             'display' : 'inherit',
             'z-index' : '2'
@@ -260,5 +354,9 @@ $(document).ready(function(){
     });
     
     // Idle Timer
-    
+    $(document).bind("idle.idleTimer", function(){
+    // function you want to fire when the user goes idle
+        $(location).attr('href', 'index.html');
+    });
+    $.idleTimer(3*60000);
 });
